71064c32b05bac6c6a8dfb9c0eff502549986c8a
caseyfee <caseyfee3@gmail.com>
2023-04-04 17:28:15 -0700

    added signup form handler

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
 
-router.use('/homeRoutes', homeRoutes);
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,13 @@
 const loginFormHandler = async (event) => {
-  // TODO: Add a comment describing the functionality of this statement
+  // Stop the browser from submitting the form so we can do so with JavaScript
   event.preventDefault();
 
-  // TODO: Add a comment describing the functionality of these expressions
+  // Gather the data from the form elements on the page
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
-    // TODO: Add a comment describing the functionality of this expression
+    // Send the e-mail and password to the server
     const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
@@ -22,6 +22,33 @@ const loginFormHandler = async (event) => {
   }
 };
 
+const signUpFormHandler = async (event) => {
+  // Stop the browser from submitting the form so we can do so with JavaScript
+  event.preventDefault();
+
+  // Gather the data from the form elements on the page
+  const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (name && email && password) {
+    // Send the e-mail and password to the server
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log in');
+    }
+  }
+};
+
 document
   .querySelector('.login-form')
+  .querySelector('.signup-form')
+
   .addEventListener('submit', loginFormHandler);
